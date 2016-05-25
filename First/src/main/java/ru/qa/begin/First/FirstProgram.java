59a85e861fc3af2d860736da0088ee85040d8c1e
agroshev <Andrey.Groshev@t-systems.ru>
2016-05-25 17:11:22 +0300

    package was created

--- First/src/main/java/ru/qa/begin/First/FirstProgram.java
+++ First/src/main/java/ru/qa/begin/First/FirstProgram.java
@@ -1,6 +1,8 @@
+package ru.qa.begin.First;
+
 public class FirstProgram {
   public static void main(String[] args) {
-System.out.println("Hello? Universe");
+System.out.println("Hello? Universe ");
 }
 
 }
