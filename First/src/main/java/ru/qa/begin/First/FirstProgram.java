49e6d106387a70e1604a4e17f631e2835e9ef1fe
agroshev <Andrey.Groshev@t-systems.ru>
2016-05-26 18:42:47 +0300

    some functions was added

--- First/src/main/java/ru/qa/begin/First/FirstProgram.java
+++ First/src/main/java/ru/qa/begin/First/FirstProgram.java
@@ -2,7 +2,31 @@ package ru.qa.begin.First;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello? Universe ");
+        hello("world");
+        String somebody = "Universe";
+
+        double len = 5;
+        System.out.println("square of cube of size " + len + " = " + area(len));
+        System.out.println("Hello?" + somebody + "!");
+
+        double a = 4;
+        double b = 6;
+        System.out.println("square of piped of side " + a + " and " + b+ " = " + area(a,b));
     }
+
+    public static void hello(String somebody) {
+
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a*b;
+
+    }
+
 
 }
