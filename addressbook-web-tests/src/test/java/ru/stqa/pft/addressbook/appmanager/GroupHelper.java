c5c35db1801e3da7ab6d32b69b5090fd4d696b9f
agroshev <Andrey.Groshev@t-systems.ru>
2016-06-16 13:35:02 +0300

    part 2, lection 11

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -0,0 +1,41 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by agroshev on 15.06.2016.
+ */
+public class GroupHelper extends HelperBase {
+
+    public GroupHelper(FirefoxDriver wd) {
+        super(wd);
+    }
+
+    public void returnToGroupPage() {
+        click(By.linkText("group page"));
+    }
+
+    public void submitGroupCreation() {
+        click(By.name("submit"));
+    }
+
+    public void fillGroupForm(GroupData groupData) {
+        type(By.name("group_name"), groupData.getName());
+        type(By.name("group_header"), groupData.getHeader());
+        type(By.name("group_footer"), groupData.getFooter());
+    }
+
+    public void InitGroupCreation() {
+        click(By.name("new"));
+    }
+
+    public void deleteSelectedGroups() {
+        click(By.name("delete"));
+    }
+
+    public void selectGroup() {
+        click(By.name("selected[]"));
+    }
+}
